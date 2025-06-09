f136f92891b218eeb05aa82f9d8647357bc4e73b
theprice1 <66714035+theprice1@users.noreply.github.com>
2025-06-09 23:02:01 +0100

    End of day updates  images, README, config

---
+++ mobile/app.config.js
@@ -0,0 +1,34 @@
+export default {
+  expo: {
+    name: "WhoDoggy",
+    slug: "whodoggy",
+    version: "1.0.0",
+    sdkVersion: "51.0.0",
+    platforms: ["ios", "android", "web"],
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    }
+  }
+};
+// This configuration file is used by Expo to set up the mobile application.
+// It includes settings for the app's name, version, icon, splash screen, and platform-specific configurations. 
