45c0e4e77fb70c7f79465f6e6d37be43e553077a
theprice1 <66714035+theprice1@users.noreply.github.com>
2025-07-19 11:39:46 +0100

    App.tsx workspace

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,19 @@
-// apps/web/src/App.tsx
-
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './pages/Home/HomeScreen';
+import SearchPage from './pages/Search/SearchPage';
+
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900">
-      <HomeScreen navigation={undefined} />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100 text-gray-900">
+        <Routes>
+          <Route path="/" element={<HomeScreen navigation={undefined} />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
