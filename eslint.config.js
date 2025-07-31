9a5624fd4e4020d2ca5a89c3434d8f86cec84e07
theprice1 <66714035+theprice1@users.noreply.github.com>
2025-08-01 00:30:59 +0100

    package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-// eslint.config.js
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import parser from '@typescript-eslint/parser';  // import parser module
 
 export default [
   {
-    ignores: ['dist', 'node_modules'],
+    ignores: ['dist', 'node_modules', 'eslint.config.js'],
     files: ['**/*.ts'],
     languageOptions: {
-      parser: '@typescript-eslint/parser',
+      parser,  // use the imported parser object here
       parserOptions: {
         project: './tsconfig.json',
         sourceType: 'module',
