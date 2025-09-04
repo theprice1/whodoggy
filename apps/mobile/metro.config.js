d7f55a7d9c97c0884c54aacc11f3b08261693069
theprice1 <66714035+theprice1@users.noreply.github.com>
2025-09-04 11:04:06 +0100

    fixing problems

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,20 +1,6 @@
 // apps/mobile/metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("node:path");
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
-
-const config = getDefaultConfig(projectRoot);
-
-// Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
-
-// Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-	path.resolve(projectRoot, "node_modules"),
-	path.resolve(workspaceRoot, "node_modules"),
-];
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
-
