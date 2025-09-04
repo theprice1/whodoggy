9708e86f9b35300af052ce2108d41caf86355886
theprice1 <66714035+theprice1@users.noreply.github.com>
2025-09-04 17:00:56 +0100

    fixing problems

--- packages/shared/src/firebase/firebaseAdmin.ts
+++ packages/shared/src/firebase/firebaseAdmin.ts
@@ -1,12 +1,13 @@
-// Firebase Admin SDK
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from '../../../../backend/firebase/serviceAccountKey.json';
+import serviceAccount from "./serviceAccountKey.json";
 
-// Initialize app only once
-const app = getApps().length === 0 ? initializeApp({ credential: cert(serviceAccount) }) : undefined;
+const app = initializeApp({
+  credential: cert(serviceAccount as any),
+});
 
-// Exports
-export const adminAuth = getAuth();
-export const adminDb = getFirestore();
+const adminAuth = getAuth(app);
+const adminDb = getFirestore(app);
+
+export { app, adminAuth, adminDb };
